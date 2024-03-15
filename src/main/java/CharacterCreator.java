bab44d4b2ad80582544c487bd1deb51992ef285a
Emil <emjo2210@student.miun.se>
2024-03-15 13:57:27 +0100

    Updated javadoc for placeholder class

--- src/main/java/CharacterCreator.java
+++ src/main/java/CharacterCreator.java
@@ -1,20 +1,45 @@
+/**
+ * This class is responsible for creating a character based on the user input.
+ * @author Emil Jönsson
+ */
 public class CharacterCreator {
-    
+
+    /**
+     * Constructor for the character creator class.
+     */
     public CharacterCreator() { }
 
-
-    public boolean selectClass(int i) {
+    /**
+     * Selects a class based on the user input.
+     * @param input number input from the user.
+     * @return true if the input is valid, false if not.
+     */
+    public boolean selectClass(int input) {
         return false;
     }
 
-    public boolean selectName(String testName) {
+    /**
+     * Selects a name based on the user input.
+     * @param input the name input from the user.
+     * @return true if the input is valid, false if not.
+     */
+    public boolean selectName(String input) {
         return false;
     }
 
-    public boolean reRollStatSheet(int i) {
+    /**
+     * Re-rolls a stat based on the user input.
+     * @param input number input from the user.
+     * @return true if the input is valid, false if not.
+     */
+    public boolean reRollStatSheet(int input) {
         return false;
     }
 
+    /**
+     * Returns the stat sheet of the current character.
+     * @return stat sheet object.
+     */
     public Object getStatSheet() {
         return null;
     }
