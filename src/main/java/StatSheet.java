0512fd3d3b56be98058c7f1b43632f72c472d66d
Emil <emjo2210@student.miun.se>
2024-03-15 13:33:52 +0100

    Added tests for stat sheet class. Added stat sheet placeholder class and methods.

---
+++ src/main/java/StatSheet.java
@@ -0,0 +1,17 @@
+public class StatSheet {
+    public boolean reRollSelected(String strength) {
+        return false;
+    }
+
+    public int getReRollAmount() {
+        return 0;
+    }
+
+    public int getStat(String stat) {
+        return 0;
+    }
+
+    public boolean setStat(String stat, int value) {
+        return false;
+    }
+}
---
+++ src/test/java/TestStatSheet.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TestStatSheet {
+
+    public static final int ReRollAmount = 3;
+    private final StatSheet statSheet = new StatSheet();
+
+    /**
+     * Tests if a valid stat can be re-rolled.
+     */
+    @Test
+    public void testReRollSelected() {
+        Assertions.assertTrue(statSheet.reRollSelected("Strength"));
+        Assertions.assertEquals(ReRollAmount - 1, statSheet.getReRollAmount());
+    }
+
+    /**
+     * Tests if an invalid stat is handled correctly.
+     */
+    @Test
+    public void testInvalidStatReRoll() {
+        Assertions.assertFalse(statSheet.reRollSelected("InvalidStat"));
+    }
+
+    /**
+     * Tests if attempting to get an invalid stat returns -1.
+     */
+    @Test
+    public void testGettingInvalidStat() {
+        Assertions.assertEquals(-1, statSheet.getStat("InvalidStat"));
+    }
+
+    /**
+     * Tests if setting a valid stat is updated correctly.
+     */
+    @Test
+    public void testSetStat() {
+        int expected = 10;
+        Assertions.assertTrue(statSheet.setStat("Strength", expected));
+        Assertions.assertEquals(expected, statSheet.getStat("Strength"));
+    }
+
+    /**
+     * Tests if setting an invalid stat is handled correctly.
+     */
+    @Test
+    public void testSetInvalidStat() {
+        int expected = 10;
+        Assertions.assertFalse(statSheet.setStat("InvalidStat", expected));
+    }
+}
