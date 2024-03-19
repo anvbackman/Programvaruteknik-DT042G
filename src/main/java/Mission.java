c7522fb384d25a569e42c5f85cb509660d148e38
Emil <emjo2210@student.miun.se>
2024-03-19 15:55:08 +0100

    Accessor methods added
    Added access to difficulty and next fork values, added javadoc to existing functions.

--- src/main/java/Mission.java
+++ src/main/java/Mission.java
@@ -14,16 +14,25 @@ public class Mission {
 
     private int currentFork;
     private final int length;
-    private final int difficulty;
+    private final String difficulty;
+    private HashMap<Integer, List<String>> mission;
 
-    public Mission(final int difficulty, final int missionLength) {
+    /**
+     * Constructor for the mission class.
+     * @param difficulty the difficulty of the mission.
+     * @param missionLength the length of the mission.
+     */
+    public Mission(final String difficulty, final int missionLength) {
         this.length = Randomizer.rollMissionLength(missionLength);
         this.difficulty = difficulty;
         this.currentFork = 0;
     }
 
+    /**
+     * Generates a mission and saves it to a hashmap.
+     */
     public final void generateMission() {
-        HashMap<Integer, List<String>> mission = new HashMap<>();
+        mission = new HashMap<>();
 
         for (int i = 1; i <= length; i++) {
             mission.put(i, generateFork(i));
@@ -31,6 +40,11 @@ public class Mission {
         mission.forEach((key, value) -> System.out.println("Fork " + key + ": " + value));
     }
 
+    /**
+     * Generates a fork of the mission.
+     * @param fork the fork number.
+     * @return a list of mission types.
+     */
     private List<String> generateFork(final int fork) {
         if (fork == 1) {
             return List.of(Constants.MISSION_TYPE_COMBAT);
@@ -50,7 +64,32 @@ public class Mission {
         }
     }
 
-    public final int getDifficulty() {
+    /**
+     * Returns the difficulty of the mission.
+     * @return the difficulty of the mission as string value.
+     */
+    public final String getDifficulty() {
         return difficulty;
+    }
+
+    /**
+     * Returns the length of the mission.
+     * @return the length of the mission as integer value.
+     */
+    public final int getLength() {
+        return length;
+    }
+
+    /**
+     * Returns the next fork in the mission or an empty list if there are no more forks.
+     * @return the next fork in the mission as a list of mission types.
+     */
+    public final List<String> getNextFork() {
+        List <String> fork = new ArrayList<>();
+        if (currentFork < length) {
+            fork = mission.get(currentFork);
+        }
+        currentFork++;
+        return fork;
     }
 }
