2e65e5b71a7159eeb8d90055adb07e2acda9fc3d
Maro2207 <Maro2207@student.miun.se>
2024-03-20 16:24:34 +0100

    # Added Brutalize
    ## Concrete class to make a brutal attack for barbarians.

--- src/main/java/abilities/Brutalize.java
+++ src/main/java/abilities/Brutalize.java
@@ -4,7 +4,7 @@ import support.Randomizer;
 
 /**
  * Brutalize ability for the Barbarian class, this ability is used to deal damage to a single target.
- * The damage is calculated by rolling a d10 for every level the character has - will be changed later..'
+ * The damage is calculated by rolling a d10 for every level the character has - will be changed later.'
  */
 public class Brutalize extends BaseAbility {
 
