e27215997b7a545d7225993231b629a7e2510839
Maro2207 <Maro2207@student.miun.se>
2024-03-20 15:55:08 +0100

    # Added Smite
    ## Concrete class to cast Smite, will be further expanded when combat mechanics have been constructed.

---
+++ src/main/java/abilities/Smite.java
@@ -0,0 +1,47 @@
+package abilities;
+
+import support.Randomizer;
+
+/**
+ * Smite ability for the Paladin class, this ability is used to deal damage to a single target.
+ * The damage is calculated by rolling a d10 for every level the character has.
+ */
+public class Smite extends BaseAbility{
+
+    /**
+     * Constructor for the Smite class.
+     */
+    public Smite() {
+            super("Smite");
+        }
+
+    /**
+     * Method that calculates the damage of the ability.
+     * @param CharLevel the level of the character using the ability.
+     * @return the damage dealt by the ability.
+     */
+    private int damageCalc(int CharLevel) {
+
+            int result = Randomizer.rollD10(CharLevel);
+            return result;
+
+        }
+
+    /**
+     * Method that returns the number of targets the ability can hit.
+     * @return
+     */
+    private int getTargets(){
+            return 1;
+        }
+
+    /**
+     * Method that executes the ability.
+     * @param target the target of the ability.
+     * @param charLevel the level of the character using the ability.
+     */
+    public void execute(int target, int charLevel) {
+            int targets = getTargets();
+            int damage = damageCalc(charLevel);
+        }
+}
