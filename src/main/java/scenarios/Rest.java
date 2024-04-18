40450cca684cf695f55d2b4e72e04d1682e80a72
Maro2207 <Maro2207@student.miun.se>
2024-04-18 14:11:52 +0200

    # Implemented rest scenarios, where events can occur
    ## This will be used for Rest class, which handles rest encounters.

--- src/main/java/scenarios/Rest.java
+++ src/main/java/scenarios/Rest.java
@@ -3,6 +3,7 @@ package scenarios;
 import character.Hero;
 import creator.Mission;
 import support.Constants;
+import support.Randomizer;
 
 import java.util.Scanner;
 
@@ -57,21 +58,54 @@ public class Rest implements Encounter {
      * Generates the encounters for the easy difficulty.
      */
     private void generateEncountersForMissionEasy() {
-        System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        int Danger = Randomizer.rollD20(1);
+        if (Danger <= 5) {
+            System.out.println("During your rest you are ambushed by a group of goblins!");
+            System.out.println("You fight them off, but you are injured during the fight.");
+            hero.reduceHealth(Randomizer.rollD4(1));
+        } else {
+            System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        }
+        System.out.println("The rest of the night is peaceful, and you embark further on your journey.");
     }
 
     /**
      * Generates the encounters for the medium difficulty.
      */
     private void generateEncountersForMissionMedium() {
-        System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        int Danger = Randomizer.rollD20(1);
+        if(Danger <= 5){
+            System.out.println(" During the night, someone seems to have stolen some of your supplies.");
+            hero.removeConsumable(hero.getConsumables().get(Randomizer.rollD4(1) - 1));
+            hero.addGold(-10);
+        }
+        if(Danger <= 10){
+            System.out.println("During your rest you are ambushed by a group of Kobolds!");
+            System.out.println("You fight them off, but you are injured during the fight.");
+            hero.reduceHealth(Randomizer.rollD4(2));
+        } else {
+            System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        }
+        System.out.println("The rest of the night is peaceful, and you embark further on your journey.");
     }
 
     /**
      * Generates the encounters for the hard difficulty.
      */
     private void generateEncountersForMissionHard() {
-        System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        int Danger = Randomizer.rollD20(1);
+        if(Danger <= 5){
+            System.out.println(" During the night, one of the survivors in the village must have taken some of your money");
+            hero.addGold(-10);
+        }
+        if(Danger <= 15){
+            System.out.println("During your rest you are ambushed by a group of Zombies!");
+            System.out.println("You fight them off, but you are injured during the fight.");
+            hero.reduceHealth(Randomizer.rollD4(3));
+        } else {
+            System.out.println("You encounter nothing unusual, and enjoy a peaceful rest.");
+        }
+        System.out.println("The rest of the night is peaceful, and you embark further on your journey.");
     }
 
 
